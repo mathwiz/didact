d9104f27605fc43a3002941eb562460b4bb40fcf
mathwiz <mathwiz@gmail.com>
2019-02-20 09:25:45 -0500

    lesson 6)

---
+++ getting-started-with-matlab7/ch2/Lesson06.m
@@ -0,0 +1,20 @@
+pwd
+
+dir
+
+% mkdir lesson06dir
+
+% cd lesson06dir
+ls
+
+% x = 1:100;
+% y = log10(x);
+% save xydatafile.mat x y
+ls
+
+what
+
+% cd ..
+% pwd
+% copyfile unit_circle.jpg lesson06dir
+% ls lesson06dir
