deb6dcebae05df072ee24e708f034ecacaf50881
mathwiz <mathwiz@gmail.com>
2019-02-24 11:00:32 -0500

    functional

--- data-science-from-scratch/ch2/functional.py
+++ data-science-from-scratch/ch2/functional.py
@@ -1,4 +1,4 @@
-from functools import partial
+from functools import partial, reduce
 
 
 def exp(base, power):
@@ -22,3 +22,18 @@ print(list(powers_of_two))
 list_powers_of_two = partial(map, two_to_the)
 print(list(list_powers_of_two(xs)))
 
+
+print([x for x in xs if x % 2 == 0])
+print(list(filter(lambda x: x % 2 == 0, xs)))
+list_evener = partial(filter, lambda x: x % 2 == 0)
+print(list(list_evener(xs)))
+
+
+print(reduce(lambda x, y: x * y, xs))
+list_product = partial(reduce, lambda x, y: x * y)
+print(list_product(xs))
+
+
+print(reduce(lambda x, y: x + square_of(y), xs))
+list_sum_of_squares = partial(reduce, lambda x, y: x + square_of(y))
+print(list_sum_of_squares(xs))
