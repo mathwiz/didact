9afb80f8b71ef703bfd45d2049cdbf6200fe5d28
mathwiz <mathwiz@gmail.com>
2019-03-18 15:40:16 -0400

    ch 7 mini

--- common-lisp/key-change.lisp
+++ common-lisp/key-change.lisp
@@ -12,3 +12,28 @@
                    (A 10) 
                    (A-SHARP 11) 
                    (B 12)))
+
+
+(defun numbers (notes)
+  (mapcar (lambda (x) (cadr (assoc x note-table))) notes))
+
+
+(defun notes (nums)
+  (let ((rev-note-table (mapcar #'reverse note-table)))
+    (mapcar (lambda (x) (cadr (assoc x rev-note-table))) nums)))
+
+
+(defun raise (amount nums)
+  (mapcar (lambda (x) (+ x amount)) nums))
+
+
+(defun normalize (nums)
+  (let ((normer (lambda (x)
+                  (cond ((< x 1) (+ x 12))
+                        ((> x 12) (mod x 12))
+                        (t x)))))
+    (mapcar normer nums)))
+
+
+(defun transpose (steps notes)
+  (notes (normalize (raise steps (numbers notes)))))
