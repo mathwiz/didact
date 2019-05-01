a1cfeba04c176f6a0e5821f1ab064c84d6d0f059
mathwiz <mathwiz@gmail.com>
2019-05-01 11:15:34 -0400

    differentiation

--- quantitative-ecologist/symbolic_differentiation.R
+++ quantitative-ecologist/symbolic_differentiation.R
@@ -9,4 +9,13 @@ b<- 0.1
 x<- 100
 eval(D(expression(a*x / (1 + b*x)), "x"))
 
+D(expression(5 + 5*cos(pi * (x-2) / 12)), "x")
+
+D(D(expression(-3*x^2 + 5*x - 2), "x"), "x")
+
+                                        # Partial differentiation
+
+D(expression(x / (x + y)), "x")
+
+D(D(expression(x / (x + y)), "x"), "x")
 
