10829c7c4719185cf075342f93ba76d49e9173b6
mathwiz <yohanlee@yahoo.com>
2019-05-08 15:04:19 -0400

    stats

--- data-science-from-scratch/statfuncs.py
+++ data-science-from-scratch/statfuncs.py
@@ -57,6 +57,27 @@ def variance(x):
 	return vecmat.sum_of_squares(deviations) / (n - 1)
 
 
+def standard_deviation(x):
+	return math.sqrt(variance(x))
+
+
+def interquartile_range(x):
+	return quantile(x, 0.75) - quantile(x, 0.25)
+
+
+def covariance(x, y):
+	n = len(x)
+	return vecmat.dot(de_mean(x), de_mean(y)) / (n - 1)
+
+
+def correlation(x, y):
+	stdev_x = standard_deviation(x)
+	stdev_y = standard_deviation(y)
+	if stdev_x > 0 and stdev_y > 0:
+		return covariance(x, y) / stdev_x / stdev_y
+	else:
+		return 0
+
 
 
 
--- data-science-from-scratch/statfuncs_test.py
+++ data-science-from-scratch/statfuncs_test.py
@@ -24,6 +24,19 @@ class MyTestCase(unittest.TestCase):
     def test4(self):
         np.testing.assert_almost_equal(stats.variance(w), .916, 3)
 
+    def test5(self):
+        self.assertEqual(stats.quantile(w, 0.25), 1)
+        self.assertEqual(stats.quantile(w, 0.75), 3)
+        self.assertEqual(stats.interquartile_range(w), 2)
+
+    def test6(self):
+        self.assertEqual(stats.covariance([1,1,1,1,1], x), 0)
+        self.assertEqual(stats.covariance([4,5,6,0,0], x), 1.25)
+
+    def test7(self):
+        np.testing.assert_almost_equal(stats.correlation([4,5,6,0,0], x), 0.528, 3)
+        np.testing.assert_almost_equal(stats.correlation([-2,-1,0,1,2], [2,1,0,1,2]), 0.0, 3)
+
 
 
 if __name__ == '__main__':
