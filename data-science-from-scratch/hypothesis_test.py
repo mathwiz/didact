45b4d4f12373c22fa7da959a8bee81960b7b381d
mathwiz <yohanlee@yahoo.com>
2019-05-22 15:53:59 -0400

    prob dists

--- data-science-from-scratch/hypothesis_test.py
+++ data-science-from-scratch/hypothesis_test.py
@@ -1,26 +1,23 @@
 import unittest
+import numpy as np
 import probability as funcs
 
 
 class MyTestCase(unittest.TestCase):
     def test1(self):
-        print(funcs.normal_approximation_to_binomial(100, .5))
+        self.assertEqual(funcs.normal_approximation_to_binomial(100, .5), (50.0, 5.0))
 
 
-    # def test2(self):
-    #     self.assertEqual(funcs.uniform_cdf(.5), .5)
-    #     self.assertEqual(funcs.uniform_cdf(1.5), 1)
-    #     self.assertEqual(funcs.uniform_cdf(0.0), 0)
+    def test2(self):
+        np.testing.assert_almost_equal(funcs.normal_cdf(1.69), .95, 2)
+        np.testing.assert_almost_equal(funcs.normal_cdf(-1.69), .05, 2)
+        np.testing.assert_almost_equal(funcs.normal_probability_below(1.69), .95, 2)
 
-    # def test3(self):
-    #     np.testing.assert_almost_equal(funcs.normal_pdf(2), .06, 2)
-    #     np.testing.assert_almost_equal(funcs.normal_pdf(0), .40, 2)
-    #     np.testing.assert_almost_equal(funcs.normal_pdf(-2), .05, 2)
 
-    # def test4(self):
-    #     np.testing.assert_almost_equal(funcs.normal_cdf(2), .97, 2)
-    #     np.testing.assert_almost_equal(funcs.normal_cdf(0), .5, 2)
-    #     np.testing.assert_almost_equal(funcs.normal_cdf(-2), .03, 2)
+    def test3(self):
+        np.testing.assert_almost_equal(funcs.normal_probability_between(-1.96, 1.96), .95, 2)
+        np.testing.assert_almost_equal(funcs.normal_probability_outside(-1.96, 1.96), .05, 2)
+
 
     # def test5(self):
     #     self.assertTrue(funcs.bernoulli_trial(.5) == 0 or funcs.bernoulli_trial(.5) == 1)
--- data-science-from-scratch/probability.py
+++ data-science-from-scratch/probability.py
@@ -80,3 +80,16 @@ def normal_approximation_to_binomial(n, p):
 normal_probability_below = normal_cdf
 
 
+def normal_probability_above(lo, mu=0, sigma=1):
+	return 1 - normal_cdf(lo, mu, sigma)
+
+
+def normal_probability_between(lo, hi, mu=0, sigma=1):
+	return normal_cdf(hi, mu, sigma) - normal_cdf(lo, mu, sigma)
+
+
+def normal_probability_outside(lo, hi, mu=0, sigma=1):
+	return 1 - normal_probability_between(lo, hi, mu, sigma)
+
+
+
