52206d440543db6f1b8c42ad287ec5e51593cfa6
mathwiz <yohanlee@yahoo.com>
2019-06-17 10:31:37 -0400

    data science

--- data-science-from-scratch/gradient_descent.py
+++ data-science-from-scratch/gradient_descent.py
@@ -4,6 +4,10 @@ from collections import Counter
 import numpy as np
 
 
+def sum_of_squares(v):
+        return sum(v_i**2 for v_i in v)
+
+
 def difference_quotient(f, x, h):
 	return (f(x + h) - f(x)) / h
 
