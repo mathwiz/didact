273e74bb18d6a47e6c44adb90e1b2717d6d2bc42
Yohan Lee <yohanlee@yahoo.com>
2019-08-23 19:55:13 -0400

    using statements

--- accelerated-c++/c01/FrameInput.cpp
+++ accelerated-c++/c01/FrameInput.cpp
@@ -9,29 +9,34 @@
 #include <iostream>
 #include <string>
 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
 int FrameInput()
 {
-    std:: cout << "Please enter your first name: ";
-    std::string name; //define name
-    std::cin >> name; //read into name
+    cout << "Please enter your first name: ";
+    string name; //define name
+    cin >> name; //read into name
     
     //build the message that we intend to write
-    const std::string greeting = "Greetings, " + name + "!";
+    const string greeting = "Greetings, " + name + "!";
     
     //build the second and fourth lines of the output
-    const std::string spaces(greeting.size(), ' ');
-    const std::string second = "* " + spaces + " *";
+    const string spaces(greeting.size(), ' ');
+    const string second = "* " + spaces + " *";
     
     //build the first and fifth lines of the output
-    const std::string first(second.size(), '*');
+    const string first(second.size(), '*');
     
     //write it all
-    std::cout << std::endl;
-    std::cout << first << std::endl;
-    std::cout << second << std::endl;
-    std::cout << "* " << greeting << " *" << std::endl;
-    std::cout << second << std::endl;
-    std::cout << first << std::endl;
+    cout << endl;
+    cout << first << endl;
+    cout << second << endl;
+    cout << "* " << greeting << " *" << endl;
+    cout << second << endl;
+    cout << first << endl;
     return 0;
 }
 
