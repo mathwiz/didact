f4eb25fa49e31a6890b83ea84c60d93a0a8ee6a4
mathwiz <mathwiz@gmail.com>
2019-09-05 15:24:23 -0400

    tail recursion

--- common-lisp/tail-recursion.lisp
+++ common-lisp/tail-recursion.lisp
@@ -1,5 +1,17 @@
 (defun count-up (n)
-  (defun recurse (cnt)
-    (cond ((> cnt n) nil)
-	  (t (cons cnt (recurse (+ cnt 1))))))
-  (recurse 1))
+  (labels ((recurse (cnt)
+	     (cond ((> cnt n) nil)
+		   (t (cons cnt (recurse (+ cnt 1)))))))
+    (recurse 1)))
+
+
+(defun count-up-tail (n)
+  (labels ((recurse (cnt acc)
+	     (cond ((< cnt 1) acc)
+		   (t (recurse (- cnt 1) (cons cnt acc))))))
+    (recurse n nil)))
+
+
+(defun fact (n)
+  (cond ((= n 0) 1)
+	(t n)))
