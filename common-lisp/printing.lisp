1b41b5deff11541a9f12ba8ab3b122a79fa5799b
mathwiz <mathwiz@gmail.com>
2019-09-30 16:59:19 -0400

    printing

--- common-lisp/printing.lisp
+++ common-lisp/printing.lisp
@@ -1,15 +1,30 @@
 (defun dot-prin1 (objs)
-  (cond ((null objs) (format t "~S" nil))
+  (cond ((null objs) (format t "NIL"))
 	((listp objs)
 	 (print-open)
-	 (cond
-	   ((listp (car objs)) (dot-prin1 (car objs)) (format t " . ") (dot-prin1 (cdr objs)))
-	   (t (format t "~S . " (car objs)) (dot-prin1 (cdr objs))))
+	 (dot-prin1 (car objs))
+	 (print-dot)
+	 (dot-prin1 (cdr objs))
 	 (print-close))
 	(t (format t "~S" objs))))
+
+(defun hybrid-prin1 (objs)
+  (cond ((null objs) (format t ""))
+	((listp objs)
+	 (print-open)
+	 (hybrid-prin1 (car objs))
+	 (print-dot)
+	 (hybrid-prin1 (cdr objs))
+	 (print-close))
+	(t (format t "~S" objs))))
+
+
 
 (defun print-open ()
   (format t "("))
 
 (defun print-close ()
   (format t ")"))
+
+(defun print-dot ()
+  (format t " . "))
