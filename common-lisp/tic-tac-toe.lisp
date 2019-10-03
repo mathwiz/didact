b17dd476774c13ff49789e8fffd74d980d52d698
mathwiz <mathwiz@gmail.com>
2019-10-03 11:49:10 -0400

    tic tac toe)

--- common-lisp/tic-tac-toe.lisp
+++ common-lisp/tic-tac-toe.lisp
@@ -3,16 +3,16 @@
 ;; 4 | 5 | 6
 ;; 7 | 8 | 9
 ;;
-;; X = 1, O = 0, empty = -1
+;; X = 10, O = 1, empty = 0
 ;;
 
 
 (defun make-board ()
-  (list 'board -1 -1 -1 -1 -1 -1 -1 -1 -1))
+  (list 'board 0 0 0 0 0 0 0 0 0))
 
 (defun convert-to-letter (val)
-  (cond ((equal val 1) "X")
-	((equal val 0) "O")
+  (cond ((equal val 10) "X")
+	((equal val 1) "O")
 	(t " ")))
 
 (defun print-row (x y z)
@@ -46,3 +46,11 @@
 (setf *computer* 1)
 (setf *opponent* 0)
 
+(defvar *triplets*)
+(setf *triplets*
+      '((1 2 3) (4 5 6) (7 8 9)
+	(1 4 7) (2 5 8) (3 6 9)
+	(1 5 9) (3 5 7)))
+
+(defun sum-triple (t)
+  0)
