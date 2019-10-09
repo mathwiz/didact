ff5ed787f94204191a4d6905651991e12ea66d9e
Yohan Lee <yohanlee@yahoo.com>
2019-10-08 20:56:52 -0400

    wizard

--- land-of-lisp/wizard-adventure.scm
+++ land-of-lisp/wizard-adventure.scm
@@ -9,4 +9,11 @@
   (cadr (assoc location nodes)))
   
    
- 
+(define *edges* '((living-room (garden west door)
+                    			(attic upstairs ladder))
+                  (garden (living-room east door))
+                  (attic (living-room downstairs ladder))))
+
+(define (describe-path edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
