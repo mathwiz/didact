4b96d3476cea94fbd402e97fbb92a2bd140a2ecc
mathwiz <mathwiz@gmail.com>
2019-10-15 11:44:30 -0400

    on-lisp ch 5

---
+++ on-lisp/destructive-equivalents.lisp
@@ -0,0 +1,9 @@
+;; Allow the making of destructive versions of existing system or user functions
+
+(defvar *!equivs* (make-hash-table))
+
+(defun ! (fn)
+  (or (gethash fn *!equivs*) fn))
+
+(defun def! (fn fn!)
+  (setf (gethash fn *!equivs*) fn!))
