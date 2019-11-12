f25e17eedf9721d84a50051f06ed8169d9d893a6
mathwiz <mathwiz@gmail.com>
2019-11-12 15:37:29 -0500

    on lisp

---
+++ on-lisp/conditional-evaluation.lisp
@@ -0,0 +1,14 @@
+(defmacro if3 (test t-case nil-case ?-case)
+  `(case ,test
+     ((nil) ,nil-case)
+     (?     ,?-case)
+     (t     ,t-case)))
+
+
+(defmacro nif (expr pos zero neg)
+  (let ((g (gensym)))
+    `(let ((,g ,expr))
+       (cond ((plusp ,g) ,pos)
+	     ((zerop ,g) ,zero)
+	     (t ,neg)))))
+
