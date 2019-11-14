f95001e7ee51806f16174df6256096916212d75b
mathwiz <mathwiz@gmail.com>
2019-11-14 10:18:20 -0500

    on lisp

---
+++ on-lisp/iteration-macros.lisp
@@ -0,0 +1,20 @@
+(defmacro while (test &body body)
+  `(do ()
+       ((not ,test))
+     ,@body))
+
+
+(defmacro till (test &body body)
+  `(do ()
+       (,test)
+     ,@body))
+
+
+(defmacro for ((var start stop) &body body)
+  (let ((gstop (gensym)))
+    `(do ((,var ,start (1+ ,var))
+	  (,gstop ,stop))
+	 ((> ,var ,gstop))
+       ,@body)))
+
+
