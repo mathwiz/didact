aaab483f8806074dc47ad6442171c4aaef72183f
mathwiz <mathwiz@gmail.com>
2019-11-15 16:26:47 -0500

    on lisp

---
+++ on-lisp/setf-macros.lisp
@@ -0,0 +1,8 @@
+(defmacro _f (op place &rest args)
+  (multiple-value-bind (vars forms var set access)
+      (get-setf-method place)
+    `(let* (,@(mapcar #'list vars forms)
+	    (,(car var) (,op ,access ,@args)))
+       ,set)))
+
+
