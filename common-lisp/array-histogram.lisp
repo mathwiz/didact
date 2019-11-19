dd572521811a57bbcf0df508921e3cb8469ffc9f
mathwiz <mathwiz@gmail.com>
2019-11-19 15:42:36 -0500

    common lisp

--- common-lisp/array-histogram.lisp
+++ common-lisp/array-histogram.lisp
@@ -4,5 +4,19 @@
 
 (defun new-histogram (n)
   (setf *hist-array* (make-array n :initial-element 0))
-  (setf *total-points* n))
+  (setf *total-points* 0))
+
+
+(defun record-value (n)
+  (cond (nil nil)
+        (t nil)))
+
+
+(defun print-hist-line (n)
+  nil)
+
+
+(defun print-histogram ()
+  (dotimes (i (length *hist-array*))
+    (print-hist-line i)))
 
