d9a5747c208fffee8d3718298f1aa3749a863265
Yohan Lee <yohanlee@yahoo.com>
2019-11-21 09:58:55 -0500

    deep learning

---
+++ deep-learning-kelleher/backprop.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import examples
+
+
+class Network:
+
+    def __init__(self, layer_sizes):
+        self.layers = layer_sizes
+
+    def run(self):
+        pass
+
+
+def relu(val):
+    return val if val > 0 else 0
+    
+    
+def identity(val):
+    return val
+
+
+def produce_output(val, size=1):
+    return np.repeat(val, size)
+
+
+def sum(input, weights):
+    return np.sum(input * weights)
+    
+
+def initial_weights(n, max=1):
+    return np.random.uniform(low=0, high=max, size=n)    
+
+
+# initialize
+
