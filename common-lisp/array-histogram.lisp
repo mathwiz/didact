cb74631d4142beb4b908b6e98977f2047918f192
mathwiz <mathwiz@gmail.com>
2019-11-22 10:04:47 -0500

    common lisp

--- common-lisp/array-histogram.lisp
+++ common-lisp/array-histogram.lisp
@@ -1,6 +1,9 @@
 (defvar *hist-array*)
 (defvar *total-points*)
 
+(defun get-hist ()
+  *hist-array*)
+
 
 (defun new-histogram (n)
   (setf *hist-array* (make-array n :initial-element 0))
@@ -8,15 +11,23 @@
 
 
 (defun record-value (n)
-  (cond (nil nil)
-        (t nil)))
+  (let ((h *hist-array*))
+    (cond ((or (> n 0) (< n (length h))) (setf (aref h n) (1+ (aref h n))))
+          (t (error "Input must be between 0 and ~S" (length h))))))
 
 
 (defun print-hist-line (n)
-  nil)
+  (let ((val (aref *hist-array* n)))
+    (progn
+      (format t "~2S [~3S] " n val)
+      (dotimes (i val)
+        (format t "*")))))
 
 
 (defun print-histogram ()
   (dotimes (i (length *hist-array*))
-    (print-hist-line i)))
+    (progn
+      (print-hist-line i)
+      (format t "~%")
+      )))
 
