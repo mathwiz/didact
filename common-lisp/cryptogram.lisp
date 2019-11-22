292939db986ef1bf6ad75841e59d2097e7b0c77c
mathwiz <mathwiz@gmail.com>
2019-11-22 15:28:57 -0500

    common lisp

---
+++ common-lisp/cryptogram.lisp
@@ -0,0 +1,23 @@
+(setf crypto-text
+      '("zj ze kljjls jf slapzi ezvlij pib kl jufwxuj p hffv jupi jf"
+        "enlpo pip slafml pvv bfwkj"))
+
+
+(defvar *encipher-table*)
+(defvar *decipher-table*)
+
+(setf *encipher-table* (make-hash-table))
+(setf *decipher-table* (make-hash-table))
+
+
+(defun make-substitution (a b)
+  nil)
+
+(defun solve (text)
+  (let ((spacer "-----------------"))
+    (progn
+      (format t spacer)
+      (format t spacer)
+      )))
+
+
