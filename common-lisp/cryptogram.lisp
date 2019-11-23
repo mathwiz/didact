267322779d8647821be66e5238901facb3c1a642
mathwiz <mathwiz@gmail.com>
2019-11-23 16:44:33 -0500

    common lisp

--- common-lisp/cryptogram.lisp
+++ common-lisp/cryptogram.lisp
@@ -10,8 +10,47 @@
 (setf *decipher-table* (make-hash-table))
 
 
-(defun make-substitution (a b)
+(defun make-substitution (clear crypt)
+  (progn
+    (setf (gethash crypt *decipher-table*) clear)
+    (setf (gethash clear *encipher-table*) crypt)
+    ))
+
+
+(defun undo-substitution (letter)
+  (progn
+    (setf (gethash letter *decipher-table*) nil)
+    (setf (gethash letter *encipher-table*) nil)
+    ))
+
+
+(defun clear ()
+  (progn
+    (clrhash *decipher-table*)
+    (clrhash *encipher-table*)
+    ))
+
+
+(defun decipher (c)
+  (gethash c *decipher-table*))
+
+
+(defun replace (old new pos str)
   nil)
+
+
+(defun decipher-string (s)
+  (let* ((len (length s))
+         (result (make-string len :initial-element #\Space)))
+    (progn
+      (dotimes (i len)
+        (let* ((c (nth i s))
+               (replacement (decipher c)))
+          (setf result "replacementofresultati")
+          ))
+       result)
+    ))
+
 
 (defun solve (text)
   (let ((spacer "-----------------"))
