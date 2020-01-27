555493b2d24d7da441676b142f4807634a3ddf85
Yohan Lee <yohanlee@yahoo.com>
2020-01-27 10:04:50 -0500

    on lisp ch 15

--- on-lisp/anaphoric-list-recursion.lisp
+++ on-lisp/anaphoric-list-recursion.lisp
@@ -1,3 +1,5 @@
+;; Require list-recursor.lisp
+
 ;; Using 'it' for current car of list and 'rec' for recursive call
 
 (defmacro alrec (rec &optional base)
@@ -9,3 +11,45 @@
 
 (defmacro on-cdrs (rec base &rest lsts)
   `(funcall (alrec ,rec #'(lambda () ,base)) ,@lsts))
+
+(defun our-copy-list (lst)
+  (on-cdrs (cons it rec) nil lst))
+
+(defun our-remove-duplicates (lst)
+  (on-cdrs (adjoin it rec) nil lst))
+
+(defun our-find-if (lst)
+  (on-cdrs (if (funcall fn it) it rec) nil lst))
+
+(defun our-some (fn lst)
+  (on-cdrs (or (funcall fn it) rec) nil lst))
+
+(defun unions (&rest sets)
+  (on-cdrs (union it rec) (car sets) (cdr sets)))
+
+(defun intersections (&rest sets)
+  (unless (some #'null sets)
+    (on-cdrs (intersection it rec) (car sets) (cdr sets))))
+
+(defun differences (set &rest outs)
+  (on-cdrs (set-difference rec it) set outs))
+
+(defun maxmin (args)
+  (when args
+    (on-cdrs (multiple-value-bind (mx mn) rec
+               (values (max mx it) (min mn it)))
+             (values (car args) (car args))
+             (cdr args))))
+
+;; Testing
+(format t "Testing anaphoric-list-recursion~%")
+(write (unions '(a b) '(b c) '(c d e)))
+
+(format t "~%")
+(write (intersections '(a b) '(b c) '(c d b)))
+
+(format t "~%")
+(write (differences '(a b c d e) '(a f) '(d)))
+
+(format t "~%")
+(write (maxmin '(3 4 2 8 5 1 6 7)))
