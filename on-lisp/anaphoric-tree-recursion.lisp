9f28d7b2d913f3754697216a604c7ea5446f86f8
mathwiz <mathwiz@gmail.com>
2020-02-07 16:38:11 -0500

    on lisp ch 15

--- on-lisp/anaphoric-tree-recursion.lisp
+++ on-lisp/anaphoric-tree-recursion.lisp
@@ -1,3 +1,36 @@
-;; Require tree-recursor.lisp, single.lisp
+;; Require tree-recursor2.lisp, single.lisp
+(load "tree-recursor2.lisp")
+(load "single.lisp")
 
+(defmacro atrec (rec &optional (base 'it))
+  (let ((lfn (gensym)) (rfn (gensym)))
+    `(trec #'(lambda (it ,lfn ,rfn)
+               (symbol-macrolet ((left (funcall ,lfn))
+                                 (right (funcall ,rfn)))
+                 ,rec))
+           #'(lambda (it) ,base))))
+
+
+(defmacro on-trees (rec base &rest trees)
+  `(funcall (atrec ,rec ,base) ,@trees))
+
+
+;;;; Examples
+
+(defun our-copy-tree (tree)
+  (on-trees (cons left right) it tree))
+
+
+(defun count-leaves (tree)
+  (on-trees (+ left (or right 1)) 1 tree))
+
+
+(defun flatten (tree)
+  (on-trees (nconc left right) (mklist it) tree))
+
+
+(defun rfind-if (fn tree)
+  (on-trees (or left right)
+            (and (funcall fn it) it)
+            tree))
 
