b33aa4fee97e86e79d1b1f051baaa527e8b6b08d
Yohan Lee <yohanlee@yahoo.com>
2020-02-19 17:43:23 -0500

    on lisp ch 18

--- on-lisp/destructuring.lisp
+++ on-lisp/destructuring.lisp
@@ -4,3 +4,28 @@
     (print x)
     (print y)
     (print z)))
+
+
+;; Same concept but works with any kind of sequence
+(defmacro dbind (pat seq &body body)
+  (let ((gseq (gensym)))
+    `(let ((,gseq ,seq))
+       ,(dbind-ex (destruc pat gseq #'atom) body))))
+
+
+(dbind (a (b c) d) '(1 #(2 3) 4)
+       (progn
+         (print a)
+         (print b)
+         (print c)
+         (print d)))
+
+
+(dbind (a (b . c) &rest d) '(1 "fribble" 2 3 4)
+       (progn
+         (print a)
+         (print b)
+         (print c)
+         (print d)))
+
+
