438dae3d08182762cd4e9a7aaba1d7749297e561
Yohan Lee <yohanlee@yahoo.com>
2020-02-26 16:20:19 -0500

    on lisp ch 18

--- on-lisp/pattern-matching.lisp
+++ on-lisp/pattern-matching.lisp
@@ -48,6 +48,7 @@
   (and (symbolp x) (eq (char (symbol-name x) 0 #\?))))
 
 
+
 ;; examples
 (print (match '(p a b c a) '(p ?x ?y c ?x)))
 
@@ -60,5 +61,71 @@
 (let ((m (match '(a ?x b) '(_ 1 _))))
   (print m))
 
+
+
+;; Fast matching operator
+(defmacro if-match (pat seq then &optional else)
+  `(let ,(mapcar #'(lambda (v) `(,v ',(gensym)))
+                 (vars-in pat #'simple?))
+     (pat-match ,pat ,seq ,then ,else)))
+
+
+(defmacro pat-match (pat seq then else)
+  (if (simple? pat)
+      (match1 `((,path ,seq)) then else)
+      (with-gensyms (gseq gelse)
+        `(labels ((,gelse () ,else))
+           ,(gen-match (cons (list gseq seq)
+                             (destruc pat gseq #'simple?))
+                       then
+                       `(,gelse))))))
+
+
+(defun simple? (x) (or (atom x) (eq (car x) 'quote)))
+
+
+(defun gen-match (refs then else)
+  (if (null refs)
+      then
+      (let ((then (gen-match (cdr refs) then else)))
+        (if (simple? (caar refs))
+            (match1 refs then else)
+            (gen-match (car refs) then else)))))
+
+
+(defun match1 (refs then else)
+  (dbind ((pat expr) . rest) refs
+         (cond ((gensym? pat)
+                `(let ((,pat ,expr))
+                   (if (and (typep ,pat 'sequence)
+                            ,(length-test pat rest))
+                       ,then
+                       ,else)))
+               ((eq pat '_) then)
+               ((var? pat)
+                (let ((ge (gensym)))
+                  `(let ((,ge ,expr))
+                     (if (or (gensym? ,pat) (equal ,pat ,ge))
+                         (let ((,pat ,ge)) ,then)
+                         ,else))))
+               (t `(if (equal ,pat ,expr) ,then ,else)))))
+
+
+(defun gensym? (a)
+  (and (sumbolp a) (not (symbol-package s))))
+
+
+(defun length-test (pat rest)
+  (let ((fin (caadar (last rest))))
+    (if (or (consp fin) (eq fin 'elt))
+        `(= (length ,pat) ,(length rest))
+        `(> (length ,pat) ,(- (length rest) 2)))))
+
+
+
+;; exmaples. What's wrong?
+(let ((n 3)
+      (m (if-match (?x n 'n '(a b)) '(1 3 n (a b)) ?x)))
+  (print m))
 
 (print 'pattern-matching)
