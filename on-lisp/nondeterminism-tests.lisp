69c028edff0c1b8257e497828e66303a8886d5aa
Yohan Lee <yohanlee@yahoo.com>
2020-03-02 13:36:53 -0500

    on lisp ch 22

---
+++ on-lisp/nondeterminism-tests.lisp
@@ -0,0 +1,21 @@
+;; Require
+(load "continuation-passing.lisp")
+(load "nondeterminism.lisp")
+
+
+(=defun two-numbers ()
+        (choose-bind n1 '(0 1 2 3 4 5)
+                     (choose-bind n2 '(0 1 2 3 4 5)
+                                  (=values n1 n2))))
+
+
+
+(=defun parlor-trick (sum)
+        (=bind (n1 n2) (two-numbers)
+               (if (= (+ n1 n2) sum)
+                   `(the sum of ,n1 ,n2)
+                   (fail))))
+
+
+;; try it
+(print (parlor-trick 7))
