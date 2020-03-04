c178335f33792b211f6b553cd48eafd749202b13
Yohan Lee <yohanlee@yahoo.com>
2020-03-04 08:15:26 -0500

    on lisp ch 25

--- on-lisp/object-oriented-test.lisp
+++ on-lisp/object-oriented-test.lisp
@@ -10,7 +10,22 @@
 
 
 ;; try it
+(format t "~%Calculating area...~%")
 (let ((myrec (obj rectangle)))
   (setf (height myrec) 2 (width myrec) 3)
-  (print (area myrec)))
+  (format t "Area is ~D~%" (area myrec)))
 
+
+(setq filesystem (obj))
+(defmeth (backup :before) filesystem (fs)
+         (format t "Remember to mount the tape.~%"))
+(defmeth (backup) filesystem (fs)
+         (format t "Oops, deleted all your files.~%")
+         'done)
+(defmeth (backup :after) filesystem (fs)
+         (format t "Well, that was easy.~%"))
+
+
+;; try it
+(format t "~%Backing up...~%")
+(backup (obj filesystem))
