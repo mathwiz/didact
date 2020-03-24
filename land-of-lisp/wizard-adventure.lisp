716758dad27746b315bbd585fb02ca2b764b0db1
Yohan Lee <yohanlee@yahoo.com>
2020-03-24 16:16:41 -0400

    land of lisp

--- land-of-lisp/wizard-adventure.lisp
+++ land-of-lisp/wizard-adventure.lisp
@@ -66,7 +66,7 @@
         (progn
           (setf *location* (car next))
           (look))
-        '(you cannot go that way))))
+        '(you cannot go that way.))))
 
 
 (defun pickup (object)
@@ -74,7 +74,7 @@
                  (objects-at *location* *objects* *object-locations*))
          (push (list object 'body) *object-locations*)
          `(you are now carrying the ,object))
-    	(t '(you cannot get that))))
+    	(t '(you cannot get that.))))
 
 
 (defun inventory ()
@@ -95,5 +95,36 @@
              (list 'quote x)))
       (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
 
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+      (eval sexp)
+      '(i do not know that command.)))
+
+
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "() "
+                                                  (prin1-to-string lst))
+                                     'list)
+                             t
+                             nil)
+                 'string))
+  (fresh-line))
+
+
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+          (rest (cdr lst)))
+      (cond
+        ((eq item #\space) (cons item (tweak-text rest caps lit)))
+        ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+        ((eq item #\") (tweak-text rest caps (not lit)))
+        (lit (cons item (tweak-text rest nil lit)))
+        ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+        (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
 
 
