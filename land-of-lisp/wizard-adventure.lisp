4f54fe86420e531641b084c7e9069b97d244f652
Yohan Lee <yohanlee@yahoo.com>
2020-03-24 11:50:14 -0400

    land of lisp

--- land-of-lisp/wizard-adventure.lisp
+++ land-of-lisp/wizard-adventure.lisp
@@ -88,6 +88,12 @@
       (game-repl))))
 
 
+(defun game-read ()
+  (let ((cmd (read-from-string
+              (concatenate 'string "(" (read-line) ")"))))
+    (flet ((quote-it (x)
+             (list 'quote x)))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
 
 
 
