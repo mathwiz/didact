be8b85b41742f2a808c74f8ea981805b7c8be7b4
Yohan Lee <yohanlee@yahoo.com>
2020-04-22 16:38:14 -0400

    brooks

--- programming-in-common-lisp/problems01.lisp
+++ programming-in-common-lisp/problems01.lisp
@@ -1,3 +1,12 @@
-;; 1.1
+;; 1
 (defun dot-product (trip1 trip2)
-  nil)
+  (+ (* (car trip1) (car trip2))
+     (* (car (cdr trip1)) (car (cdr trip2)))
+     (* (car (cdr (cdr trip1))) (car (cdr (cdr trip2))))))
+
+
+(print (dot-product '(1.2 2.4 -0.3) '(0.0 4.5 3.8)))
+(print (dot-product '(1 3 -5) '(4 -2 -1)))
+
+
+;; 2
