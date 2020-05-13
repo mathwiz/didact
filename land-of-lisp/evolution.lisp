dd46785aa35bf18b1ae358d78bb83ab78e5fe0f5
Yohan Lee <yohanlee@yahoo.com>
2020-05-13 09:57:11 -0400

    evolution

---
+++ land-of-lisp/evolution.lisp
@@ -0,0 +1,21 @@
+(defparameter *width* 100)
+(defparameter *height* 30)
+(defparameter *jungle* '(45 10 10 10))
+(defparameter *plant-energy* 80)
+(defparameter *plants* (make-hash-table :test #'equal))
+
+
+(defun random-plant (left top width height)
+  (let ((pos (cons (+ left (random width)) (+ top (random height)))))
+    (setf (gethash pos *plants*) t)))
+
+
+(defun add-plants ()
+  (apply #'random-plant *jungle*)
+  (random-plant 0 0 *width* *height*))
+
+
+(defstruct animal x y energy dir genes)
+
+
+
