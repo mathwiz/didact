61e9f22cc5c0f8517a38500223f1cb19dbdb7e00
Yohan Lee <yohanlee@yahoo.com>
2020-06-28 16:03:18 -0400

    exceptions

--- land-of-lisp/exceptions.lisp
+++ land-of-lisp/exceptions.lisp
@@ -3,3 +3,16 @@
              (princ "Stop FOOing around, numbskull!" stream))))
 
 
+(defun bad-function ()
+  (error 'foo))
+
+
+;; Exmples to run in REPL
+(handler-case (bad-function)
+  (foo () "somebody signaled foo!")
+  (bar () "somebody signaled bar!"))
+
+
+(unwind-protect (/ 1 0)
+  (princ "I need to do this even on an error."))
+
