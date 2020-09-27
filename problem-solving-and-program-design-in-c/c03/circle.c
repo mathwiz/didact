61dbfc1510dc4da5f978b3749888a9f4dc31489e
Yohan Lee <yohanlee@yahoo.com>
2020-09-27 18:58:33 -0400

    c

--- problem-solving-and-program-design-in-c/c03/circle.c
+++ problem-solving-and-program-design-in-c/c03/circle.c
@@ -6,7 +6,19 @@
 #define PI 3.14159
 
 int main(void) {
-  
+  double radius;
+  double area;
+  double circum;
 
+  /* get the radius */
+  printf("Enter radius> ");
+  scanf("%lf", &radius);
+  
+  area = PI * radius * radius;
+  circum = 2 * PI * radius;
+
+  printf("The area is %.4f\n", area);
+  printf("The circumference is %.4f\n", circum);
+    
   return 0;
 }
