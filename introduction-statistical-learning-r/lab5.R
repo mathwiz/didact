c033c6d9ad8dffcbd61a49e439e23c19cf099041
Yohan Lee <yohanlee@yahoo.com>
2021-04-09 16:58:09 -0400

    lab5

---
+++ introduction-statistical-learning-r/lab5.R
@@ -0,0 +1,116 @@
+library(ISLR)
+
+set.seed(1)
+
+
+# Validation Set Approach
+
+train = sample(392, 196)
+
+lm.fit = lm(mpg~horsepower, data=Auto, subset=train)
+
+attach(Auto)
+
+mean((mpg - predict(lm.fit, Auto))[-train]^2)
+
+lm.fit2 = lm(mpg~poly(horsepower, 2), data=Auto, subset=train)
+
+mean((mpg - predict(lm.fit2, Auto))[-train]^2)
+
+lm.fit3 = lm(mpg~poly(horsepower, 3), data=Auto, subset=train)
+
+mean((mpg - predict(lm.fit3, Auto))[-train]^2)
+
+
+# Leave-One-Out Cross-Validation
+
+glm.fit = glm(mpg~horsepower, data=Auto)
+
+coef(glm.fit)
+
+lm.fit = lm(mpg~horsepower, data=Auto)
+
+coef(lm.fit)
+
+library(boot)
+
+glm.fit = glm(mpg~horsepower, data=Auto)
+
+cv.err = cv.glm(Auto, glm.fit)
+
+cv.err$delta
+
+cv.error = rep(0, 5)
+
+for (i in 1:5) {
+    glm.fit = glm(mpg~poly(horsepower, i), data=Auto)
+    cv.error[i] = cv.glm(Auto, glm.fit)$delta[1]
+}
+
+cv.error
+
+
+# k-Fold Cross-Validation
+
+set.seed(17)
+
+cv.error.10 = rep(0, 10)
+
+for (i in 1:10) {
+    glm.fit = glm(mpg~poly(horsepower, i), data=Auto)
+    cv.error.10[i] = cv.glm(Auto, glm.fit, K=10)$delta[1]
+}
+
+cv.error.10
+
+
+# The Bootstrap
+
+alpha.fn = function(data, index) {
+    X = data$X[index]
+    Y = data$Y[index]
+    return((var(Y) - cov(X,Y)) / (var(X) + var(Y) - 2*cov(X,Y)))
+}
+
+# test the function
+alpha.fn(Portfolio, 1:100)
+
+set.seed(1)
+
+alpha.fn(Portfolio, sample(100, 100, replace=T))
+
+# use boot library to automate sampling
+
+boot(Portfolio, alpha.fn, R=1000)
+
+
+# example of estimating the accuracy of a linear model
+
+boot.fn = function(data, index)
+    return(coef(lm(mpg~horsepower, data=data, subset=index)))
+
+boot.fn(Auto, 1:392)
+
+set.seed(1)
+
+boot.fn(Auto, sample(392, 392, replace=T))
+
+boot.fn(Auto, sample(392, 392, replace=T))
+
+boot(Auto, boot.fn, 1000)
+
+# compare result of boot to regression model std err
+
+summary(lm(mpg~horsepower, data=Auto))$coef
+
+# do again with quadratic model
+
+boot.fn = function(data, index)
+    return(coefficients(lm(mpg~horsepower+I(horsepower^2), data=data, subset=index)))
+
+set.seed(1)
+
+boot(Auto, boot.fn, 1000)
+
+
+
