d461396dcc92f6afcc9ab71523762ab9629a4af5
Yohan Lee <yohanlee@yahoo.com>
2021-07-28 15:27:34 -0400

    stick figures

--- problem-solving-and-program-design-in-c/c03/stick_figure.c
+++ problem-solving-and-program-design-in-c/c03/stick_figure.c
@@ -2,3 +2,66 @@
  * Draw a stick figure
  */
 
+#include <stdio.h>
+
+/* functions */
+
+void draw_spacer(void);
+void draw_circle(void);
+void draw_intersect(void);
+void draw_base(void);
+void draw_triangle(void);
+
+int
+main(void)
+{
+  draw_circle();
+  draw_spacer();
+  draw_triangle();
+  draw_spacer();
+  draw_intersect();
+  draw_spacer();
+
+  return 0;
+}
+
+
+void
+draw_spacer(void)
+{
+  printf("\n");
+}
+
+
+void
+draw_circle(void)
+{
+  printf("  *\n");
+  printf("*   *\n");
+  printf(" * *\n");
+}
+
+
+void
+draw_intersect(void)
+{
+  printf("   /\\\n");
+  printf("  /  \\\n");
+  printf(" /    \\\n");
+  printf("/      \\\n");
+}
+
+
+void
+draw_base(void)
+{
+  printf("--------\n");
+}
+
+
+void
+draw_triangle(void)
+{
+  draw_intersect();
+  draw_base();
+}
