d51169ce4a83673cc16566d0ec620957c0ff686d
Yohan Lee <yohanlee@yahoo.com>
2021-08-03 10:30:43 -0400

    multiple arguments

---
+++ problem-solving-and-program-design-in-c/c03/scale_factor.c
@@ -0,0 +1,34 @@
+/*
+ * Test a function taking multiple arguments.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+
+double scale(double num_to_scale, int scale_factor);
+
+
+int
+main(void)
+{
+  double num;
+  int factor;
+
+  printf("Enter a real number to scale> ");
+  scanf("%lf", &num);
+
+  printf("Enter an integer scale factor> ");
+  scanf("%d", &factor);
+  
+  printf("Result of call to scale(%.2f, %d) is %.0f\n", num, factor, scale(num, factor));
+
+  return 0;
+}
+
+
+double
+scale(double x, int n)
+{
+  return x * pow(10, n);
+}
