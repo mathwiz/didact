61d0212630403fd761fa17088d8014014d4c0c6e
Yohan Lee <yohanlee@yahoo.com>
2021-08-16 17:18:38 -0400

    ch 4

---
+++ problem-solving-and-program-design-in-c/c04/water_bill.c
@@ -0,0 +1,14 @@
+/* Computes and prints a water bill given an unpaid balance and previous
+ * and current meter readings. Bill includes a demand charge of $35.00, 
+ * a use charge of $1.10 per thousand gallons, and a surcharge of $2.00
+ * if there is an unpaid balance.
+ */
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+  return 0;
+}
