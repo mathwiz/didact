5394eafb18824b0a8b22287f4e245666565b3cd7
Yohan Lee <yohanlee@yahoo.com>
2022-03-24 13:17:59 -0400

    casio

--- chaos-fractals-and-dynamics/iterate1.py
+++ chaos-fractals-and-dynamics/iterate1.py
@@ -2,6 +2,6 @@ x0 = float(input("x0? "))
 c = float(input("c? "))
 n = int(input("n? "))
 for i in range(n):
-    x0 = c * x0 * (1-x0)
-    print(f'{i + 1} {x0}')
+  x0 = c * x0 * (1-x0)
+  print("iteration %2d: %0.4f" % (i+1, x0))
 
