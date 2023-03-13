385d32c1374860ba12f6fe88876c3cf90f4f18c5
Yohan Lee <yohanlee@yahoo.com>
2023-03-13 15:34:53 -0400

    modelling evolution

---
+++ modeling-evolution/ch1/model3.R
@@ -0,0 +1,28 @@
+rm(list=ls())
+
+Maxgen = 100
+N.Init = 20
+Max.Lambda = 2.2
+# randomize growth rate
+set.seed(100)
+Lambda = runif(Maxgen, min=0, max=Max.Lambda)
+
+Generation = seq(from=1, to=Maxgen) # vector of all the generation numbers
+Npop = matrix(0, Maxgen, 1)
+Npop[1] = N.Init
+
+i <- 1
+while (i < Maxgen && Npop[i] > 1) {
+    i <- i + 1
+    Npop[i] <- Lambda[i-1] * Npop[i-1]
+}    
+
+print("model3 finished.")
+
+# Output
+output = function() {
+    plot(Generation, Npop, xlab='Generation', ylab='Popluation size', type='l')
+    print("Final population size")
+    print(tail(Npop, 1))
+}
+
