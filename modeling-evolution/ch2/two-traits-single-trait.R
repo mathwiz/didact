a874e25216abf1b10320e75de8b15c7935eeac20
Yohan Lee <yohanlee@yahoo.com>
2023-04-03 12:26:44 -0400

    modeling evolution

--- modeling-evolution/ch2/two-traits-single-trait.R
+++ modeling-evolution/ch2/two-traits-single-trait.R
@@ -21,37 +21,38 @@ X = matrix(seq(from=0.0, to=4.0, length=N))
 W = apply(X, 1, fitness)
 
 
-## # find maximum using calculus
-## derivative = function(x) {
-##     t1 <- abs(0.4*x[2] + 0.8 - 2.0*0.4*x[1])
-##     t2 <- abs(0.4*x[1] + 0.8 - 2.0*0.4*x[2])
-##     return(t1 + t2)
-## }
-## Max.Symb = nlm(derivative, p=c(1,1))$estimate
+# find maximum using calculus
+derivative.calc = function(x) {
+    t1 <- S1 * exp(-A*x)
+    t2 <- S2 * exp(-A*(R/N - x))
+    return(t1 - t2)
+}
+Max.Symb = uniroot(derivative.calc, interval=c(0,3))$root
+Size.Second = (R/N) - Max.Symb
 
-## # find maximum numerically
-## fitness2 = function(x) {
-##     t1 <- A.xy * x[1] * x[2]
-##     t3 <- B.xy * x[1]
-##     t4 <- C.xy * x[1]^2
-##     t5 <- B.yx * x[2]
-##     t6 <- C.yx * x[2]^2
-##     w <- t1 - A.0 + t3 - t4 + t5 - t6
-##     return(-w)
-## }
+# alternative method
+derivative.symb = function(w) {
+    y <- deriv(~ N * (S1*F.Max*(1-exp(-A*x)) + S2*F.Max*(1-exp(-A*(R/N -x)))), "x")
+    x <- w
+    z <- eval(y)
+    d <- attr(z, "gradient")
+    return(d)
+}    
+Max.Symb2 = uniroot(derivative.symb, interval=c(0,3))$root
+Size.Second2 = (R/N) - Max.Symb2
 
-## Traits = nlm(fitness2, p=c(0.5,0.5))$estimate
-## Max.Num = -fitness2(Traits)
+# find maximum numerically
+Max.Num = optimize(f=fitness, interval=c(1,8), maximum=T)$maximum
+Size.Second.Num = (R/N) - Max.Num
 
 print('two-traits-single-trait done.')
 
 output = function() {
     par(mfrow=c(1,1))
     plot(X, W, type='l', xlab='Propagule size', ylab='Fitness, R0', las=1, lwd=3)
-    ## contour(Vigilance, Foraging, W, xlab='Vigilance', ylab='Foraging', las=1, lwd=3, labcex=1)
-    ## persp(Vigilance, Foraging, W, xlab='Vigilance', ylab='Foraging', zlab='Fitness', theta=50, phi=25, lwd=2)
-    ## print('Maximum using calculus')
-    ## print(Max.Symb)
-    ## print('Maximum numerically')
-    ## print(c(Traits, Max.Num))
+    print('Maximum using calculus')
+    print(c(Max.Symb, Size.Second))
+    print(c(Max.Symb2, Size.Second2))
+    print('Maximum numerically')
+    print(c(Max.Num, Size.Second.Num))
 }    
