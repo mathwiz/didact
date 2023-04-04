fc3228b73b02afb26aefb8ed0a1554cadb39ffc3
Yohan Lee <yohanlee@yahoo.com>
2023-04-04 14:09:47 -0400

    modeling evolution

--- modeling-evolution/ch2/three-traits.R
+++ modeling-evolution/ch2/three-traits.R
@@ -39,25 +39,31 @@ D = expand.grid(X, X)
 W = apply(D, 1, fitness)
 W = matrix(W, Size, Size)
 
-## # find maximum using calculus
-## derivative.calc = function(x) {
-##     t1 <- S1 * exp(-A*x)
-##     t2 <- S2 * exp(-A*(R/N - x))
-##     return(t1 - t2)
-## }
-## Max.Symb = uniroot(derivative.calc, interval=c(0,3))$root
-## Size.Second = (R/N) - Max.Symb
+# find maximum using calculus
+fitness2 = function(x) {
+    x2 <- x
+    x1 <- 10*log(S1/S2) + x2
+    x3 <- (R - N*(x1 + x2)) / N
 
-## # alternative method
-## derivative.symb = function(w) {
-##     y <- deriv(~ N * (S1*F.Max*(1-exp(-A*x)) + S2*F.Max*(1-exp(-A*(R/N -x)))), "x")
-##     x <- w
-##     z <- eval(y)
-##     d <- attr(z, "gradient")
-##     return(d)
-## }    
-## Max.Symb2 = uniroot(derivative.symb, interval=c(0,3))$root
-## Size.Second2 = (R/N) - Max.Symb2
+    if(x3 < 0) {
+        w <- 0
+    } else if(N*x1 > R) {
+        w <- 0
+    } else {
+        w1 <- N*S1*F.Max*(1-exp(-A*x1))
+        if(N*x2 > R) {
+            w <- w1
+        } else {
+            w2 <- N*S2*F.Max*(1-exp(-A*x2))
+            w3 <- N*S3*F.Max*(1-exp(-A*x3))
+            w <- w1 + w2 + w3
+        }    
+    }
+
+    print(c(x1, x2, x3))
+    return(-w)
+}    
+Max.Symb = nlm(fitness2, p=1)$estimate
 
 ## # find maximum numerically
 ## Max.Num = optimize(f=fitness, interval=c(1,8), maximum=T)$maximum
@@ -69,9 +75,8 @@ output = function() {
     par(mfrow=c(1,1))
     #contour(X, X, -W, xlab='Propagule size 1st clutch', ylab='Popagule size 2nd clutch')
     persp(X, X, W, xlab='Propagule size 1st clutch', ylab='Popagule size 2nd clutch', zlab='Fitness', theta=25, phi=25, lwd=1)
-    ## print('Maximum using calculus')
-    ## print(c(Max.Symb, Size.Second))
-    ## print(c(Max.Symb2, Size.Second2))
+    print('Maximum using calculus')
+    print(Max.Symb)
     ## print('Maximum numerically')
     ## print(c(Max.Num, Size.Second.Num))
 }    
