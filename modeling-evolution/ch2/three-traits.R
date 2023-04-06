7f6f7a177c500a7f5c59f9bae89b279b92012097
Yohan Lee <yohanlee@yahoo.com>
2023-04-06 12:14:33 -0400

    modeling evolution

--- modeling-evolution/ch2/three-traits.R
+++ modeling-evolution/ch2/three-traits.R
@@ -100,6 +100,24 @@ fitness3 = function(x) {
 }    
 Max.Symb2 = nlm(fitness3, p=1)$estimate
 
+# finding maximum numerically
+Max.Num <- nlm(fitness, p=c(0.1, 0.1))$estimate
+X1 = Max.Num[1]
+X2 = Max.Num[2]
+X3 = (R - N*(X1 + X2)) / N
+
+# brute force approach
+Brute.Size = 100
+Brute.X = seq(from=1.0, to=5.0, length=Brute.Size)
+Brute.Y = seq(from=1.0, to=5.0, length=Brute.Size)
+Brute.D = expand.grid(Brute.X, Brute.Y)
+Brute.W = apply(Brute.D, 1, function(x) { -fitness(x) })
+# find position in row with highest fitness
+Best = order(Brute.W, na.last=T, decreasing=T)
+Brute.X1 = Brute.D[Best[1],1]
+Brute.X2 = Brute.D[Best[1],2]
+Brute.X3 = (R - N*(Brute.X1 + Brute.X2)) / N
+
 
 print('three-traits done.')
 
@@ -111,4 +129,9 @@ output = function() {
     print(Max.Symb)
     print('Maximum using calculus (alternate method)')
     print(Max.Symb2)
+    print('Maximum numerically')
+    print(Max.Num)
+    print(c(X1, X2, X3))
+    print('Brute force approach')
+    print(c(Brute.X1, Brute.X2, Brute.X3, Brute.W[Best[1]]))
 }    
