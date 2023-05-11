5f9ad70f5873db44b3e44b16f851e43cfe7e925e
Yohan Lee <yohanlee@yahoo.com>
2023-05-11 14:40:50 -0400

    experiment in scope

--- modeling-evolution/ch5/scope.rkt
+++ modeling-evolution/ch5/scope.rkt
@@ -28,4 +28,22 @@
                                  (else (recur x (add1 a))))
                            )))))
     (recur 1 1)))
-(test-local-lambda)    
+(test-local-lambda)
+
+(define (test-stored-lambda)
+  (letrec ((make-funs (lambda (a acc)
+                        (cond ((= a 4) (reverse acc))
+                              (else (make-funs
+                                     (add1 a)
+                                     (cons (lambda (x) (+ (* a x) (* a 2))) acc))))))
+           (the-funs (make-funs 1 '()))
+           (recur (lambda (x funs)
+                    (cond ((= x 3) #t)
+                          (else
+                           (println ((car funs) x))
+                           (cond ((> (length funs) 1) (recur x (cdr funs)))
+                                 (else (recur (add1 x) the-funs)))
+                           )))))
+    (recur 1 the-funs)))
+(test-stored-lambda)
+
