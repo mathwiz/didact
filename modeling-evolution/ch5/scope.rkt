b766718bb812d6c509dea43f52dd87b0c327ea03
Yohan Lee <yohanlee@yahoo.com>
2023-05-11 17:13:26 -0400

    experiment in scope

--- modeling-evolution/ch5/scope.rkt
+++ modeling-evolution/ch5/scope.rkt
@@ -7,6 +7,7 @@
 ;(define (fun2 x)
 ;  (+ (* a x) b))
 
+(println "Using externally defined function that passes parameters a and b.")
 (define (all-args)
   (letrec ((recur (lambda (x a)
                  (cond ((= x 3) #t)
@@ -18,6 +19,7 @@
     (recur 1 1)))
 (all-args)
 
+(println "Using locally created lambdas. Note how context has name a.")
 (define (test-local-lambda)
   (letrec ((recur (lambda (x a)
                     (define (fun x) (+ (* a x) (* a 2)))
@@ -30,20 +32,22 @@
     (recur 1 1)))
 (test-local-lambda)
 
+(println "Using stored lambdas that capture local value b. Note how context has name a with wrong value.")
 (define (test-stored-lambda)
-  (letrec ((make-funs (lambda (a acc)
+  (letrec ((make-funs (lambda (a b acc)
                         (cond ((= a 4) (reverse acc))
                               (else (make-funs
                                      (add1 a)
-                                     (cons (lambda (x) (+ (* a x) (* a 2))) acc))))))
-           (the-funs (make-funs 1 '()))
-           (recur (lambda (x funs)
+                                     (* 2 (add1 a))
+                                     (cons (lambda (x) (+ (* a x) b)) acc))))))
+           (the-funs (make-funs 1 2 '()))
+           (recur (lambda (x a funs)
                     (cond ((= x 3) #t)
                           (else
                            (println ((car funs) x))
-                           (cond ((> (length funs) 1) (recur x (cdr funs)))
-                                 (else (recur (add1 x) the-funs)))
+                           (cond ((> a 0) (recur x (sub1 a) (cdr funs)))
+                                 (else (recur (add1 x) 2 the-funs)))
                            )))))
-    (recur 1 the-funs)))
+    (recur 1 2 the-funs)))
 (test-stored-lambda)
 
