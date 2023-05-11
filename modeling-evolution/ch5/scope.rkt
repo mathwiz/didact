66cdcd232f17140c4f6733830d1a207923d73bc9
Yohan Lee <yohanlee@yahoo.com>
2023-05-11 14:08:55 -0400

    experiment in scope

--- modeling-evolution/ch5/scope.rkt
+++ modeling-evolution/ch5/scope.rkt
@@ -3,6 +3,10 @@
 (define (fun1 x a b)
   (+ (* a x) b))
 
+; this does't compile
+;(define (fun2 x)
+;  (+ (* a x) b))
+
 (define (all-args)
   (letrec ((recur (lambda (x a)
                  (cond ((= x 3) #t)
@@ -12,4 +16,16 @@
                               (else (recur x (add1 a))))
                        )))))
     (recur 1 1)))
-(all-args)    
+(all-args)
+
+(define (test-local-lambda)
+  (letrec ((recur (lambda (x a)
+                    (define (fun x) (+ (* a x) (* a 2)))
+                    (cond ((= x 3) #t)
+                          (else
+                           (println (fun x))
+                           (cond ((= a 3) (recur (add1 x) 1))
+                                 (else (recur x (add1 a))))
+                           )))))
+    (recur 1 1)))
+(test-local-lambda)    
