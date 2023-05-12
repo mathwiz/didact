dc56e12802f48eccb572657ff5c59e1735212994
Yohan Lee <yohanlee@yahoo.com>
2023-05-12 14:55:22 -0400

    dynamic scope test

--- modeling-evolution/ch5/scope.lisp
+++ modeling-evolution/ch5/scope.lisp
@@ -1,63 +1,64 @@
-(defn fun1 [x a b]
+(defun println (s) (format t "~S ~%" s))
+
+(defun fun1 (x a b)
   (+ (* a x) b))
 
 ;; Does not compile
-;; (defn fun2 [x]
+;; (defun fun2 (x)
 ;;   (+ (* a x) b))
 
 (println "all arguments supplied")
-(defn all-args []
-  (let [looper (fn [x a]
-                 (cond (= x 3) true
-                       :else (do
-                       (println (fun1 x a (* a 2)))
-                       (cond (= a 3) (recur (inc x) 1)
-                             :else (recur x (inc a))))))
-        ]
-    (looper 1 1)))
+(defun all-args ()
+  (defun recur (x a)
+    (cond ((= x 3) t)
+          (t (progn (println (fun1 x a (* a 2)))
+                    (cond ((= a 3)
+                           (recur (1+ x) 1))
+                          (t (recur x (1+ a))))))))
+  (recur 1 1))
 (all-args)
 
+
 (println "using locally supplied lambda; context has name a")
-(defn local-lambda []
-  (let [looper (fn [x a b]
-                 (defn fun [x] (+ (* a x) b))
-                 (cond (= x 3) true
-                       :else (do
-                       (println (fun x))
-                       (cond (= a 3) (recur (inc x) 1 2)
-                             :else (let [next-a (inc a)]
-                                     (recur x next-a (* 2 next-a)))))
-                       ))
-        ]
-    (looper 1 1 2)))
+(defun local-lambda ()
+  (defun recur (x a b)
+    (defun fun (x)
+      (+ (* a x) b))
+    (cond ((= x 3) t)
+          (t (progn
+               (println (fun x))
+               (cond ((= a 3) 
+                      (recur (1+ x) 1 2) ) 
+                     (t (let ((next-a (1+ a))) 
+                          (recur x next-a (* 2 next-a)))))))))
+  (recur 1 1 2))
 (local-lambda)
 
 (println "Using stored lambdas that capture local value b. Note that context has name a with wrong value.")
+(defun stored-lambda ()
 
-(let [make-funs (fn [a b acc]
-                  (cond (= a 4) (reverse acc)
-                        :else (let [next-a (inc a)]
-                                (recur
+(defun make-funs (a b acc)
+                  (cond ((= a 4) (reverse acc)) 
+                        (t (let ((next-a (1+ a)))
+                                (make-funs
                                  next-a
                                  (* 2 next-a)
-                                 (cons (fn [x] (+ (* a x) b)) acc)))))
-      the-funs (make-funs 1 2 '())
+                                 (cons (lambda (x) (+ (* a x) b)) acc))))))
 
-      amax (dec (count the-funs))
-
-      stored-lambda-test (fn [x a funs]
-                      (cond (= x 3) true
-                            :else (do
-                                    (println ((first funs) x))
-                                    (cond (> a 0) (recur x (dec a) (rest funs))
-                                          :else (recur (inc x) amax the-funs)
-                                          )
-                                    )
-                            )
-                      )
-      ]
-
-
-    (stored-lambda-test 1 amax the-funs)
-
+ (let* (
+        (the-funs (make-funs 1 2 '()))
+        (amax (1- (length the-funs)))
+        )
+      (defun recur (x a funs)
+        (cond ((= x 3) t)
+              (t (progn
+                   (println (funcall (car funs) x))
+                   (cond ((> a 0) (recur x (1- a) (cdr funs)))
+                         (t (recur (1+ x) amax the-funs)))
+                   ))
+              )
+        )
+      (recur 1 amax the-funs )
+      )
 )
+(stored-lambda)
