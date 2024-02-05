a7d13813ec61725f77a6c2496cdca408e8da81a4
Yohan Lee <yohanlee@yahoo.com>
2024-02-05 13:27:31 -0500

    brooks

--- programming-in-common-lisp/ch01.lisp
+++ programming-in-common-lisp/ch01.lisp
@@ -1,3 +1,31 @@
+;; ************************************************
+(print '(1.1 What does Lisp look like?))
+
+(cons 'fruit
+      (cdr (subst 'banana
+                  'arrow
+                  '(time flies like an arrow))))
+
+(car (cons 'a 'b))
+(cdr (cons 'a 'b))
+
+(cons 'a 'nil)
+(cons '4 nil)
+(cons 'a (cons 'b nil))
+
+(cdr '(a b c))
+
+(list 'a)
+(list 'b nil)
+(list)
+
+(print (quote ('b nil)))
+(print (quote (cons 'a 'b)))
+
+
+;; ************************************************
+(print '(1.2 Conses and Atoms))  
+
 (setq a '(a b))
 (setq b 'x)
 (set b 'y)
@@ -6,25 +34,32 @@
 (print x)
 
 
-;; 1.5.1
+;; ************************************************
+(print '(1.3 Lists))
+
+
+;; ************************************************
+(print '(1.4 The Lisp Evaluation Rule))
+
+
+;; ************************************************
+(print '(1.5 Quote- The Evaluation Blocker))
+
 ;; compare to using setf
 (setq a '(u v w))
 (set (car (cdr a)) 'b)
 (print (cons v a))
 
 
-;; 1.5.2
 (setq a '(u v w))
 '(setq a '(x y z))
 (print a)
 
 
-;; 1.5.3
 (setq a 'a)
 (setq b 'a)
 (print (list a b 'b))
 
 
-;; 1.5.4
 (print (list (list 'a 'b) '(list 'a 'b)))
 
