1ac51081b000cf24efcabd8b225c82665634f212
Yohan Lee <yohanlee@yahoo.com>
2024-10-04 09:52:24 -0400

    brooks

--- programming-in-common-lisp/problems02.lisp
+++ programming-in-common-lisp/problems02.lisp
@@ -1,8 +1,8 @@
 (print "P2.1 Write a function ALL-LENGTH of one argument that counts the number of atoms that occur in a list at all levels.")
 
 (defun all-length (xs)
-  (if (null xs)
-      0
+  (if (atom xs)
+      1
       (+ (all-length (cdr xs)) 1)))
 
 (eq (all-length '(a b c)) 3)
@@ -11,4 +11,20 @@
 (eq (all-length '(nil nil (nil nil) nil)) 5)
 
 
+(print "P2.2 Write a function ALL-REVERSE of one argument that reverses a list at all levels and thus results in the following transformations.")
+
+(defun all-reverse (xs)
+  (if (atom xs)
+      xs
+      (all-reverse-aux (reverse xs))))
+
+(defun all-reverse-aux (xs)
+  (if (null (cdr xs))
+      (car xs)
+      (cons (all-reverse (car xs))
+            (all-reverse (cdr xs)))))
+
+(equal (all-reverse '(a b c d)) '(d c b a))
+(equal (all-reverse '(a (b c) (d e))) '((e d) (c b) a))
+(equal (all-reverse '(a (b c) d (e f g) h)) '(h (g f e) d (c b) a))
 
