442fb670cb6bf88125d8d6628422d1f89f343bc6
Yohan Lee <yohanlee@yahoo.com>
2025-01-17 15:00:45 -0500

    hanly

--- problem-solving-and-program-design-in-c/c06/arithmetic_with_common_fractions.c
+++ problem-solving-and-program-design-in-c/c06/arithmetic_with_common_fractions.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include "fractionio.h"
+#include "mathio.h"
+#include "mathlib.h"
 
 int main(void) {
-  int numerator, denominator;
+  /* first number */
+  int n1, d1;
+  /* second number */
+  int n2, d2;
+  /* answer */
+  int n_ans, d_ans;
+  /* inputs */
+  char op, again;
 
-  scan_fraction(&numerator, &denominator);
-  
-  printf("The number is %d / %d \n", numerator, denominator);
+  /* while the user wants to continue, repeat */
+  do {
+    printf("\nDo another problem? (y/n)> ");
+    scanf(" %c", &again);
+  } while (again == 'y' || again == 'Y');
 
   return 0;
 }
