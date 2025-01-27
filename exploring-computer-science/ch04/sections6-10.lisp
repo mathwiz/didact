9a32c119b0f99148b4916f06dca1e28fae304d5a
Yohan Lee <yohanlee@yahoo.com>
2025-01-27 15:17:27 -0500

    exploring

--- exploring-computer-science/ch04/sections6-10.lisp
+++ exploring-computer-science/ch04/sections6-10.lisp
@@ -4,3 +4,40 @@
 
 
 (print "4.7 Creating Lists")
+
+(list 'a 'b 'c)
+
+(append nil (list 'a 'b 'c))
+
+(defun add-to-end (it lst)
+  (append lst (list it)))
+
+(print "4.7.1 Exercises")
+
+(print "4.19 What do the following expressions evaluate to? Some of them may produce errors.")
+
+(cons 3 '(4))
+(cons '(3) '(4))
+(list 3 '(4))
+(list '(3) '(4))
+(list 3 4)
+(list '(3) 4)
+(append 3 '(4))
+(append '(3) '(4))
+
+(print "4.20 Assume that the following defines have been made:")
+
+(defvar numbers '(2 4 6))
+(defvar letters '(q e d))
+(defvar deep-list '(((13))))
+
+(print "Using only these three variables and the functions cons, list, and append, write expressions that will return the following lists:
+(2 4 6 q e d ((13)))
+((2 4 6) (q e d) ((13)))
+(2 4 6 (q e d) ((13)))
+((2 4 6) (q e d) (((13))))
+((2 4 6) q e d ((13)))
+")
+
+
+
