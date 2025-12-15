00dc0a61f4061fd2458c70829c780ad04a760d53
Yohan Lee <yohanlee@yahoo.com>
2025-12-15 15:22:53 -0500

    bioinformatics

--- bioinformatics-programming/genes.py
+++ bioinformatics-programming/genes.py
@@ -2,6 +2,26 @@ RNA_codon_table = {
 #                                Second Base
 #             U  C A G
 # U
-'UUU': 'Phe', 'UCU': 'Ser', 'UAU': 'Tyr', 'UGU': 'Cys', 
-'GUG': 'Val'
+'UUU': 'Phe', 'UCU': 'Ser', 'UAU': 'Tyr', 'UGU': 'Cys', # UxU
+'UUC': 'Phe', 'UCC': 'Ser', 'UAC': 'Tyr', 'UGC': 'Cys', # UxC
+'UUA': 'Leu', 'UCA': 'Ser', 'UAA': '---', 'UGA': '---', # UxA
+'UUG': 'Leu', 'UCG': 'Ser', 'UAG': '---', 'UGG': 'Urp', # UxG
+# C
+'CUU': 'Leu', 'CCU': 'Pro', 'CAU': 'His', 'CGU': 'Arg', # CxU
+'CUC': 'Leu', 'CCC': 'Pro', 'CAC': 'His', 'CGC': 'Arg', # CxC
+'CUA': 'Leu', 'CCA': 'Pro', 'CAA': 'Gln', 'CGA': 'Arg', # CxA
+'CUG': 'Leu', 'CCG': 'Pro', 'CAG': 'Gln', 'CGG': 'Arg', # CxG
+# A
+'AUU': 'Ile', 'ACU': 'Thr', 'AAU': 'Asn', 'AGU': 'Ser', # AxU
+'AUC': 'Ile', 'ACC': 'Thr', 'AAC': 'Asn', 'AGC': 'Ser', # AxC
+'AUA': 'Ile', 'ACA': 'Thr', 'AAA': 'Lys', 'AGA': 'Arg', # AxA
+'AUG': 'Met', 'ACG': 'Thr', 'AAG': 'Lys', 'AGG': 'Arg', # AxG
+# G
+'GUU': 'Val', 'GCU': 'Ala', 'GAU': 'Asp', 'GGU': 'Gly', # GxU
+'GUC': 'Val', 'GCC': 'Ala', 'GAC': 'Asp', 'GGC': 'Gly', # GxC
+'GUA': 'Val', 'GCA': 'Ala', 'GAA': 'Glu', 'GGA': 'Gly', # GxA
+'GUG': 'Val', 'GCG': 'Ala', 'GAG': 'Glu', 'GGG': 'Gly', # GxG
 }
+
+def rna_to_aa(codon):
+  return RNA_codon_table[codon]
--- bioinformatics-programming/test_genes.py
+++ bioinformatics-programming/test_genes.py
@@ -1,3 +1,10 @@
 import genes
 
+print(genes.rna_to_aa("UAG"))
+print(genes.rna_to_aa("AAG"))
+print(genes.rna_to_aa("UUU"))
+print(genes.rna_to_aa("GGC"))
+print(genes.rna_to_aa("GAC"))
+
+
 print("done")
