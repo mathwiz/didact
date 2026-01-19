8265e53609dad06d7887124890072edaab9ad7dd
Yohan Lee <yohanlee@yahoo.com>
2026-01-19 10:33:16 -0500

    xcode

---
+++ c++-crash-course/C++CC2/C++CC2/ch01.h
@@ -0,0 +1,12 @@
+//
+//  ch01.h
+//  C++CC2
+//
+//  Created by Yohan Lee on 1/19/26.
+//
+
+#include <string>
+
+std::string hello_phrase() {
+    return "Hello World!";
+}
---
+++ c++-crash-course/C++CC2/C++CC2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++CC2
+//
+//  Created by Yohan Lee on 1/19/26.
+//
+
+#include <iostream>
+#include "ch01.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << hello_phrase() << std::endl;
+    return EXIT_SUCCESS;
+}
