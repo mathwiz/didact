dde807418ce97f4722aef460dd9c0cbcce1563c3
Yohan Lee <yohanlee@yahoo.com>
2026-04-09 11:02:49 -0400

    01

---
+++ learn-c-the-hard-way/ex01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* the entry point */
+int main(int argc, char *argv[])
+{
+  int distance = 100; //my second comment
+  printf("You are %d miles away.\n", distance);
+  return 0;
+}
