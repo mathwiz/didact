ee0eccd707fec0647aa4a5489d42d38d9cce998f
Yohan Lee <yohanlee@yahoo.com>
2026-04-09 11:17:22 -0400

    03

---
+++ learn-c-the-hard-way/ex03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* ok to omit arguments to main  */
+int main()
+{
+  int age, height;
+  printf("I am %d years old.\n", age);
+  printf("I am %d inches tall.\n");
+  return 0;
+}
