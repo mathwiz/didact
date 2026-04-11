dd9e72ccfdf434a42b718482ca7b27e7978d4a13
Yohan Lee <yohanlee@yahoo.com>
2026-04-10 20:10:38 -0400

    15

--- learn-c-the-hard-way/ex15.c
+++ learn-c-the-hard-way/ex15.c
@@ -4,12 +4,40 @@ int main(int argc, char *argv[])
 {
   int ages[] = { 10, 12, 13, 14, 20 };
   char *names[] = {
-    "Ali", "Baboucar", "Crystal", "Diana"
+    "Ali", "Bashir", "Crystal", "Dina", "Jasmine"
   };
     
   //safely obtain the size of an array
   int count = sizeof(ages) / sizeof(int);
   int i = 0;
+
+  for (i = 0; i < count; i++)
+    printf("%s has %d years alive.\n", names[i], ages[i]);
+
+  printf("----\n");
+
+  // pointers to start of arrays
+  int *cur_age = ages;
+  char **cur_name = names;
+
+  for (i = 0; i < count; i++)
+    printf("%s has %d years alive if you believe pointers.\n", 
+           *(cur_name + i), *(cur_age + i));
+
+  printf("----\n");
+
+  for (i = 0; i < count; i++)
+    printf("%s has %d years alive if you like array access of pointers.\n", 
+           cur_name[i], cur_age[i]);
+
+  printf("----\n");
+
+  for (cur_name = names, cur_age = ages;
+       (cur_age - ages) < count; 
+       cur_name++, cur_age++) {
+    printf("(cur_age - ages) = (%p - %p) = %d\n", cur_age, ages, (cur_age - ages));
+    printf("%s has %d years alive if you like complex array access.\n", *cur_name, *cur_age);
+  }
 
   return 0;
 }
