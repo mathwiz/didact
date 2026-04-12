5b607655c5079f572f1884ce95e5eb2d7f8b103e
Yohan Lee <yohanlee@yahoo.com>
2026-04-12 13:18:31 -0400

    17

--- learn-c-the-hard-way/ex17.c
+++ learn-c-the-hard-way/ex17.c
@@ -123,6 +123,42 @@ void Database_list(struct Connection *conn) {
 }
 
 int main(int argc, char *argv[]) {
+  if (argc < 3) die("USAGE: ex17 <dbfile> <action> [action params]");
+  char *filename = argv[1];
+  char action = argv[2][0];
+  struct Connection *conn = Database_open(filename, action);
+  int id = 0;
+
+  if (argc > 3) id = atoi(argv[3]);
+  if (id >= MAX_ROWS) die("Id exceeds record count");
+
+  switch (action) {
+  case 'c':
+    Database_create(conn);
+    Database_write(conn);
+    break;
+  case 'g':
+    if (argc != 4) die("Need an Id to get");
+    Database_get(conn, id);
+    break;
+  case 's':
+    if (argc != 6) die("Need an Id, Title, Author to set");
+    Database_set(conn, id, argv[4], argv[5]);
+    Database_write(conn);
+    break;
+  case 'd':
+    if (argc != 4) die("Need an Id to delete");
+    Database_delete(conn, id);
+    Database_write(conn);
+    break;
+  case 'l':
+    Database_list(conn);
+    break;
+  default:
+    die("Invalid action: c=create, g=get, s=set, d=delete, l=list");
+  }
+
+  Database_close(conn);
 
   return 0;
 }
