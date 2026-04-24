55535a67c93bee87ad133b8ba9a8697582969ed6
Yohan Lee <yohanlee@yahoo.com>
2026-04-23 22:35:23 -0400

    script

---
+++ learn-c-the-hard-way/c-skeleton/tests/runtests.sh
@@ -0,0 +1,19 @@
+echo "Running ujnit tests:"
+
+for i in tests/*_test
+do
+  if test -f $i
+  then
+    if $VALGRIND ./$i 2>> tests/test.log
+    then
+      echo $i PASS
+    else
+      echo "ERROR in test $i: here's tests/test.log"
+      echo "------"
+      tail tests/test.log
+      exit 1
+    fi
+  fi
+done
+
+echo ""
