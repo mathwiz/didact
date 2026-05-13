1aef085692d291cbda482225b8cf0ddf16f94911
Yohan Lee <yohanlee@yahoo.com>
2026-05-13 18:04:57 -0400

    algo

--- algorithm-design/functional.R
+++ algorithm-design/functional.R
@@ -1,16 +1,17 @@
 # base functions
-rest <- function(x) { x[-1] }
-first <- function(x) { x[1] }
+"rest" <-
+function(x) { x[-1] }
 
-is.empty <- function(x) { length(x) == 0 }
+"first" <-
+function(x) { x[1] }
 
-sum.int <-
+"is.empty" <-
+function(x) { length(x) == 0 }
+
+"sum.int" <-
 function(begin, end) {
     if (begin > end)
         return (0)
     else
         return (begin + sum.int(begin + 1, end))
 }
-
-
-
